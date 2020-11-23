290ca249b1428afe2bf6f6152ec748ae8200353d
zhang song <11358995237@qq.com>
2020-11-23 20:30:18 +0800

    sql文件

--- foodie-dev-api/src/main/java/com/imooc/Application.java
+++ foodie-dev-api/src/main/java/com/imooc/Application.java
@@ -1,19 +1,26 @@
 package com.imooc;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigurationExcludeFilter;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.TypeExcludeFilter;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 /**
  * Created by Zhang Song on 2020/8/5 20:49
  */
-@SpringBootApplication
 // 扫描 mybatis 通用 mapper 所在的包
 //@MapperScan(basePackages = "com.imooc.mapper")
 //// 扫描所有包以及相关组件包
 //@ComponentScan(basePackages = {"com.imooc", "org.n3r.idworker"})
 ////@EnableTransactionManagement
 //@EnableScheduling       // 开启定时任务
+@SpringBootConfiguration
+@EnableAutoConfiguration
+@ComponentScan
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/HelloController.java
@@ -1,15 +1,31 @@
 package com.imooc.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Zhang Song on 2020/8/5 21:00
  */
 @RestController
 public class HelloController {
+    static HashMap<String, String> map1= new HashMap<String, String>(){{
+        put(""," ");
+    }};
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @PostMapping("/test")
+    public Map test(@RequestBody Map<String,Object> source){
+        System.out.println(source);
+        Map<String,Integer> map = new HashMap<>();
+        map.put("code",1);
+        return map;
     }
 }
